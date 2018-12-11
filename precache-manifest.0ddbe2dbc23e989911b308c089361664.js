72b1457923e59214a4a08a19f13bd3774721ebff
Vladislav Novik <v.novik@aisnovations.com>
2018-12-11 20:55:52 +0300

    Updates

---
+++ precache-manifest.0ddbe2dbc23e989911b308c089361664.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9e3b4a5c7559b821438",
+    "url": "/test-cabanas/static/js/runtime~main.b9e3b4a5.js"
+  },
+  {
+    "revision": "dc1904bf7fd78c273806",
+    "url": "/test-cabanas/static/js/main.dc1904bf.chunk.js"
+  },
+  {
+    "revision": "0ef1affe0291c692977d",
+    "url": "/test-cabanas/static/js/1.0ef1affe.chunk.js"
+  },
+  {
+    "revision": "dc1904bf7fd78c273806",
+    "url": "/test-cabanas/static/css/main.0b5db895.chunk.css"
+  },
+  {
+    "revision": "be5f7be0f99e6608374c98a588a881b9",
+    "url": "/test-cabanas/index.html"
+  }
+];
